08e1262f02e208a87a48eb2943b7cf0aad192901
Mahmoud Elmasry <79700816+mhmod33@users.noreply.github.com>
2025-04-09 18:02:39 +0200

    [2665] [Time Beats: 88.75%] [Memory Beats: 69.92%] - LeetPush

---
+++ Counter-II.js
@@ -0,0 +1,18 @@
+var createCounter = function(init) {
+    let Init=init
+    return{
+        increment(){
+            init++
+            return init 
+        },
+        reset(){
+            
+            return init=Init
+        },
+        decrement(){
+            init-- 
+            return init
+        },
+    }
+};
+
